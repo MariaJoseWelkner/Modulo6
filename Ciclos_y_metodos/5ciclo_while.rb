f20f46b63f02f94460c81e1514d1b1fdf73726e2
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-20 21:31:31 -0400

    agregando ejemplo while

---
+++ Ciclos_y_metodos/5ciclo_while.rb
@@ -0,0 +1,8 @@
+i= 0
+while i < 10
+   puts "Esto se mostrará 10 veces" # Código que queremos repetir.
+ i += 1 # IMPORTANTE
+end
+
+# La instrucción puts "Esto se mostrará 10 veces" se repetirá hasta que la variable i alcance el valor 10.
+# ● Para entonces, la comparación de la instrucción while se evaluará como false y saldremos del ciclo.
