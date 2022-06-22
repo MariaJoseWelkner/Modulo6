6ba706c2b03047671e24c3490113a6a30b7a9594
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-22 09:45:09 -0400

    agregando ptos

---
+++ Ciclos_y_metodos/24_ej_ptos_intercalados.rb
@@ -0,0 +1,10 @@
+# dibuje N números intercalados por puntos. Donde N es un valor ingresado por el usuario al momento de ejecutar el script.
+
+puts = "ingrese un num"
+num = ARGV[0].to_i 
+
+if num = even?
+    print == num
+else   
+    print '.'
+end
