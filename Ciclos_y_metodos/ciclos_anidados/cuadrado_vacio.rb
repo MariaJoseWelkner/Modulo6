2ceb9c06083e0b4eeb5088165e2987311aec2f53
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-28 18:53:47 -0400

    agregando cuadrado_vacio

--- Ciclos_y_metodos/ciclos_anidados/cuadrado_vacio.rb
+++ Ciclos_y_metodos/ciclos_anidados/cuadrado_vacio.rb
@@ -1,1 +1,26 @@
 # Crear el programa cuadrado_hueco.rb que al ejecutarse reciba un tamaño y dibuje un cuadrado dejando vacío el interior
+
+n = ARGV[0].to_i
+
+# parte superior
+n.times do 
+    print "*"
+end
+print "\n"
+
+
+# parte del medio
+(n-2).times do 
+    print "*"
+    (n-2).times do 
+        print " "
+    end
+    print "*"
+    print "\n"
+end
+
+
+# parte inferior
+n.times do
+    print "*"
+end 
