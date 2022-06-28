503bfda565f9a263ebb5695d29fd0e36fc27c425
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-28 18:01:19 -0400

    agregando triangulo invertido

--- Ciclos_y_metodos/ciclos_anidados/medio_triangulo.rb
+++ Ciclos_y_metodos/ciclos_anidados/medio_triangulo.rb
@@ -1,1 +1,9 @@
 # crear programa medio_triangulo que reciba el tamaño del triangulo 
+
+n = ARGV[0].to_i
+n.times do |i|
+    i.times do |j|
+    print '*'
+end 
+print "\n"
+end 
--- Ciclos_y_metodos/ciclos_anidados/metodo_con_parametro.rb
+++ Ciclos_y_metodos/ciclos_anidados/metodo_con_parametro.rb
@@ -2,3 +2,5 @@ def incrementar(numero, cantidad = 1)
     total = numero + cantidad
     puts "el resultado es #{total}"
 end
+
+# no me corre
