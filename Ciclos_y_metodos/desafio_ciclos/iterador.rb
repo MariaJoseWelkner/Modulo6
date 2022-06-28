bf2c07c8b4c7a6070c8c11f5a210f9afe27fdded
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-28 11:19:36 -0400

    agregando iterador

---
+++ Ciclos_y_metodos/desafio_ciclos/iterador.rb
@@ -0,0 +1,18 @@
+# con n.times seria:
+
+i = 0 
+49.times  do
+puts "iteracion #{i}"
+i = i + 1
+end 
+
+
+
+# con while seria:
+=begin
+i = 0
+while i<50
+    puts "iteracion #{i}"
+    i = i + 1
+end 
+=end 
