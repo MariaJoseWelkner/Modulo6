ffe2612e84e4efb273f2f4b7cd278b3ab8effaee
MariaJoseWelkner <josewelkner@gmail.com>
2022-06-30 18:35:04 -0400

    agregando mapycollect

---
+++ Arreglos_y_archivos/archivo_para_leer.rb/mapycollect.rb
@@ -0,0 +1,6 @@
+# map y collect son lo mismo
+# para aplicar una operacion a cada elemento del string
+
+a = [1,2,3,4,5,6,7]
+print b = a.collect {|e| e * 2}
+
--- Arreglos_y_archivos/archivo_para_leer.rb/usos.map.rb
+++ Arreglos_y_archivos/archivo_para_leer.rb/usos.map.rb
@@ -1,4 +1,5 @@
 # transformar datos de un tipo a otro:
+
 =begin
 array = ['1', '2', '3', '4']
 result = array.map {|x| x.to_i}
@@ -7,14 +8,21 @@ print result
 =end 
 
 # operar sobre todos los datos:
+
 =begin
 tiempos = [10000, 50000, 3000, 21000]
 print tiempos.map {|x| x / 1000}
 =end 
 
 # transformar todos los elementos de un arreglo de nombres a minusculas
+=begin
 nombres = ['Voleta', 'Andino', 'Clemente', 'Pia', 'Ray', 'Camilo']
 print nombres.map {|x| x.downcase}
 
 # contar la cantidad de letras que tiene cada nombre
 print largos = nombres.map {|x| x.length}
+=end 
+
+# reemplazar datos:
+array = [1,2,3,4]
+print result = array.map {|x| x<2 ? x:2}
